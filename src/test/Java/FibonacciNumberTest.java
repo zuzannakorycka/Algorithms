60aa38cf31623085d10e336b616fda2d4c6ede19
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-07-31 21:14:40 +0200

    Added new test shouldReturnEightForParameterSixInMethodFibonacciRecursively in class FibonacciNumberTest.

--- src/test/Java/FibonacciNumberTest.java
+++ src/test/Java/FibonacciNumberTest.java
@@ -1,2 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class FibonacciNumberTest {
+
+    FibonacciNumber fibonacciNumber = new FibonacciNumber();
+    int result;
+
+    @Test
+    public void shouldReturnEightForParameterSixInMethodFibonacciRecursively(){
+        result = fibonacciNumber.fibonacciRecursively(6);
+        assertEquals(8, result);
+    }
+
 }
