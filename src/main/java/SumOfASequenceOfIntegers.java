99d809d604adee9766b9080c420eb0fa853a0d18
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-08-01 22:01:20 +0200

    Added new method sumRecursively in class SumOfASequenceOfIntegers.

--- src/main/java/SumOfASequenceOfIntegers.java
+++ src/main/java/SumOfASequenceOfIntegers.java
@@ -1,3 +1,8 @@
 public class SumOfASequenceOfIntegers {
 
+    public int sumRecursively(int n){
+        if (n<0) throw new IllegalArgumentException("Only positive number.");
+        else if (n>0) return n+sumRecursively(n);
+        return 0;
+    }
 }
