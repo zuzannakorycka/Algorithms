7b520bc6aa3b7e07981ac3d4e4b00cadbdac2ea3
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-08-01 14:11:28 +0200

    Added method primeNumberRecursively in class PrimeNumber.

--- src/main/java/PrimeNumber.java
+++ src/main/java/PrimeNumber.java
@@ -1,10 +1,16 @@
 public class PrimeNumber {
 
-    public boolean primeNumber(int n) {
+    public boolean primeNumberIteratively(int n) {
         int count=0;
         for (int i=2; i<Math.sqrt(n); i++)
             if (n%i==0) count++;
         if (count==0) return true;
         else return false;
+    }
+
+    public boolean primeNumberRecursively(int n, int i){
+        if (i==1) return true;
+        else if (n%i==0) return false;
+        else return primeNumberRecursively(n, i-1);
     }
 }
