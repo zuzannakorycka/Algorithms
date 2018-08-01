5a5229a7388c983272e52037b8fc919496b54421
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-08-01 13:21:24 +0200

    Added new method primeNumberIteratively.

--- src/main/java/PrimeNumber.java
+++ src/main/java/PrimeNumber.java
@@ -1,2 +1,10 @@
 public class PrimeNumber {
+
+    public boolean primeNumber(int n) {
+        int count=0;
+        for (int i=2; i<Math.sqrt(n); i++)
+            if (n%i==0) count++;
+        if (count==0) return true;
+        else return false;
+    }
 }
