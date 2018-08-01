bf3b1ecbd775465102b9bed1f7cc69cc7bec8f19
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-08-01 13:47:49 +0200

    Added new test shouldReturnTrueForNumberFiveInMethodIteratively in class PrimeNumberTest. If method returns true, it means that n is a prime number.

--- src/test/java/PrimeNumberTest.java
+++ src/test/java/PrimeNumberTest.java
@@ -1,5 +1,15 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PrimeNumberTest {
 
+    PrimeNumber primeNumber = new PrimeNumber();
+    boolean result;
+
+    @Test
+    public void shouldReturnTrueForNumberFiveInMethodIteratively(){
+        result = primeNumber.primeNumberIteratively(5);
+        assertTrue(result);
+    }
 }
