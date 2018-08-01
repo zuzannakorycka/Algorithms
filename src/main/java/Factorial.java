4e6b1448947dee3a9b90f8a1b810de16032471f7
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-08-01 21:18:24 +0200

    Added method factorialIteratively in class Factorial.

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -1,8 +1,15 @@
 public class Factorial {
 
-    public int factorialRecursively(int n){
-        if (n==0) return 1;
-        return n*factorialRecursively(n-1);
+    public int factorialRecursively(int n) {
+        if (n == 0) return 1;
+        return n * factorialRecursively(n - 1);
     }
 
+    public int factorialIteratively(int n) {
+        int score = 1;
+        for (int i = 1; i <= n; i++) {
+            score += i;
+        }
+        return score;
+    }
 }
