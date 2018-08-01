81645694251d04003ca2990ca612887b0707edb5
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-08-01 12:52:15 +0200

    Added new test shouldReturnThreeForTwelveAndThreeInMethodRecursively in test class GreatestCommonDivisorTest.

--- src/test/java/GreatestCommonDivisorTest.java
+++ src/test/java/GreatestCommonDivisorTest.java
@@ -3,4 +3,14 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class GreatestCommonDivisorTest {
+
+    GreatestCommonDivisor greatestCommonDivisor = new GreatestCommonDivisor();
+    int result;
+
+    @Test
+    public void shouldReturnThreeForTwelveAndThreeInMethodRecursively(){
+        result = greatestCommonDivisor.gcdRecursively(12,3);
+        assertEquals(3, result);
+    }
+
 }
