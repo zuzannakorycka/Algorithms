a10db02755f630d8f4b56838387e876e56816c19
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-08-02 13:01:33 +0200

    Added new test shouldThrowIllegalArgumentExceptionForNegativeParametersInMethod in class test SumOfTheDigitsOfTheIntegerTest.

--- src/test/java/SumOfTheDigitsOfTheIntegerTest.java
+++ src/test/java/SumOfTheDigitsOfTheIntegerTest.java
@@ -1,5 +1,14 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SumOfTheDigitsOfTheIntegerTest {
 
+    SumOfTheDigitsOfTheInteger sumOfTheDigitsOfTheInteger = new SumOfTheDigitsOfTheInteger();
+    int result;
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowIllegalArgumentExceptionForNegativeParametersInMethod(){
+        sumOfTheDigitsOfTheInteger.sumOfIntegerRecursively(-5);]\
+    }
 }
