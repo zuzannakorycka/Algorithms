3d9088e8055d6915b39a55c74bce0258e940353a
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-08-02 12:10:35 +0200

    Added new method sumIteratively in class SumOfASequenceOfIntegers.

--- src/main/java/SumOfASequenceOfIntegers.java
+++ src/main/java/SumOfASequenceOfIntegers.java
@@ -5,4 +5,14 @@ public class SumOfASequenceOfIntegers {
         else if (n>0) return n+sumRecursively(n-1);
         return 0;
     }
+
+    public int sumIteratively(int n){
+        int s = 0;
+        if (n<0) throw new IllegalArgumentException("Only positive number.");
+        else {
+            for (int i = 1; i<=n; i++)
+                s+=i;
+            return s;
+        }
+    }
 }
