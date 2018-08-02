068a017bb420a78ed069b484fb0554788e4983a4
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-08-02 11:51:19 +0200

    Modified method sumRecursively in class SumOfASequenceOfIntegers. Added new test shouldReturnSixForThreeInMethodRecursively in test class SumOfASequenceOfIntegers.

--- src/main/java/SumOfASequenceOfIntegers.java
+++ src/main/java/SumOfASequenceOfIntegers.java
@@ -2,7 +2,7 @@ public class SumOfASequenceOfIntegers {
 
     public int sumRecursively(int n){
         if (n<0) throw new IllegalArgumentException("Only positive number.");
-        else if (n>0) return n+sumRecursively(n);
+        else if (n>0) return n+sumRecursively(n-1);
         return 0;
     }
 }
--- src/test/java/SumOfASequenceOfIntegersTest.java
+++ src/test/java/SumOfASequenceOfIntegersTest.java
@@ -5,10 +5,16 @@ import static org.junit.Assert.*;
 public class SumOfASequenceOfIntegersTest {
 
     SumOfASequenceOfIntegers sum = new SumOfASequenceOfIntegers();
+    int result;
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowIllegalArgumentExceptionOnNegativeParameters() {
         sum.sumRecursively(-4);
     }
 
+    @Test
+    public void shouldReturnSixForThreeInMethodRecursively(){
+        result = sum.sumRecursively(3);
+        assertEquals(6, result);
+    }
 }
