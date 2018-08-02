e694d0a7e6e2a75d668785fec1b476456d54270f
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-08-02 12:40:49 +0200

    Added new method sumOfIntegerRecursively in class SumOfTheDigitsOfTheInteger.

--- src/main/java/SumOfTheDigitsOfTheInteger.java
+++ src/main/java/SumOfTheDigitsOfTheInteger.java
@@ -1,2 +1,8 @@
 public class SumOfTheDigitsOfTheInteger {
+
+    public int sumOfIntegerRecursively(int n){
+        if (n<0) throw new IllegalArgumentException("Only positive number.");
+        else if (n>0)return n%10 + sumOfIntegerRecursively(n/10);
+        return 0;
+    }
 }
