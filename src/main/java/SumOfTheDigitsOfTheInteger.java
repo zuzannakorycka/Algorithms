9296cba32e8118664eb035b1b6710cbe9743b55a
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-08-02 13:50:54 +0200

    Added new method sumOfIntegerIteratively in class SumOfTheDigitsOfTheInteger.

--- src/main/java/SumOfTheDigitsOfTheInteger.java
+++ src/main/java/SumOfTheDigitsOfTheInteger.java
@@ -1,8 +1,23 @@
 public class SumOfTheDigitsOfTheInteger {
 
-    public int sumOfIntegerRecursively(int n){
-        if (n<0) throw new IllegalArgumentException("Only positive number.");
-        else if (n>0)return n%10 + sumOfIntegerRecursively(n/10);
+    public int sumOfIntegerRecursively(int n) {
+        if (n < 0) throw new IllegalArgumentException("Only positive number.");
+        else if (n > 0) return n % 10 + sumOfIntegerRecursively(n / 10);
         return 0;
+    }
+
+    public int sumOfIntegerIteratively(int n) {
+        int s = 0;
+        int c = 0;
+        String n_string = String.valueOf(n);
+        if (n < 0) throw new IllegalArgumentException("Only positive number.");
+        else if (n > 0) {
+            for (int i = 0; i < n_string.length(); i++) {
+                c = n % 10;
+                n = n / 10;
+                s += c;
+            }
+        }
+        return s;
     }
 }
