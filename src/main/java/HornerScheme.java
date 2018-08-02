008a9d1dabe4ff31fb9d2947952590919e8fd9a9
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-08-02 18:22:14 +0200

    Added new method in class HornerScheme.

--- src/main/java/HornerScheme.java
+++ src/main/java/HornerScheme.java
@@ -1,2 +1,7 @@
 public class HornerScheme {
+
+    public int horner(int x[], int degree, int a){
+        if (degree == 0) return x[0];
+        return a*horner(x, degree-1, a) + x[degree];
+    }
 }
