0bc469326371f4e5f8f01abf9a6c00f5d6c81815
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-08-02 15:07:53 +0200

    Added method exponentiationRecursively in class Exponentiation.

--- src/main/java/Exponentiation.java
+++ src/main/java/Exponentiation.java
@@ -1,2 +1,7 @@
 public class Exponentiation {
+
+    public int exponentiationRecursively(int n, int e) {
+        if (e == 0) return 1;
+        else return n * exponentiationRecursively(n, e - 1);
+    }
 }
