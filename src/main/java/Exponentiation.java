6f88c521a56b03a6d7a240946f5b877e06af1f3b
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-08-02 18:07:18 +0200

    Added method exponentiationIteratively and changed method exponentiationRecursively in class Exponentiation.

--- src/main/java/Exponentiation.java
+++ src/main/java/Exponentiation.java
@@ -1,8 +1,24 @@
 public class Exponentiation {
 
     public double exponentiationRecursively(double n, double e) {
-        if (e == 0) return 1;
-        else if (e>0 )return n * exponentiationRecursively(n, e - 1);
-        else return exponentiationRecursively(1/n, -e);
+        if (e > 0) return n * exponentiationRecursively(n, e - 1);
+        else if (e < 0) return exponentiationRecursively(1 / n, -e);
+        else return 1;
+    }
+
+    public double exponentiationIteratively(double n, double e) {
+        double score = 1;
+        if (e > 0) {
+            while (e > 0) {
+                score = n * score;
+                e--;
+            }
+        } else if (e < 0) {
+            while (e < 0) {
+                score = 1 / n * score;
+                e++;
+            }
+        }
+        return score;
     }
 }
