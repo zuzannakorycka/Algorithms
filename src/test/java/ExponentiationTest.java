a18e2f49d7e6ae4567fdcf02d9271bb09cfd2a6a
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-08-02 16:25:24 +0200

    Added new test shouldReturnEightForParametersTwoAndThreeInMethodRecursively in  test class ExponentiationTest.

--- src/test/java/ExponentiationTest.java
+++ src/test/java/ExponentiationTest.java
@@ -6,4 +6,10 @@ public class ExponentiationTest {
 
     Exponentiation exponentiation = new Exponentiation();
     int result;
+
+    @Test
+    public void shouldReturnEightForParametersTwoAndThreeInMethodRecursively(){
+        result = exponentiation.exponentiationRecursively(2,3);
+        assertEquals(8,result);
+    }
 }
