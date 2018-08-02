5e5a4395b728f1c5549c9339bbd0a8cf2ae5774f
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-08-02 17:50:22 +0200

    Added variable DELTA to all tests in class ExponentiationTest and added test shouldReturnReverseNAndPositiveEInFunctionRecursively.

--- src/test/java/ExponentiationTest.java
+++ src/test/java/ExponentiationTest.java
@@ -5,23 +5,30 @@ import static org.junit.Assert.*;
 public class ExponentiationTest {
 
     Exponentiation exponentiation = new Exponentiation();
-    int result;
+    double result;
+    private static final double DELTA = 1e-15;
 
     @Test
     public void shouldReturnEightForParametersTwoAndThreeInMethodRecursively(){
         result = exponentiation.exponentiationRecursively(2,3);
-        assertEquals(8,result);
+        assertEquals(8,result, DELTA);
     }
 
     @Test
     public void shouldReturnOneForExponentEEqualsZeroInFunctionRecursively(){
         result = exponentiation.exponentiationRecursively(2,0);
-        assertEquals(1,result);
+        assertEquals(1,result, DELTA);
     }
 
     @Test
     public void shouldReturnNegativeNumberInFunctionRecursively(){
         result = exponentiation.exponentiationRecursively(-2, 3);
-        assertEquals(-8,result);
+        assertEquals(-8,result, DELTA);
+    }
+
+    @Test
+    public void shouldReturnReverseNAndPositiveEInFunctionRecursively(){
+        result = exponentiation.exponentiationRecursively(2, -1);
+        assertEquals(0.5, result, DELTA);
     }
 }
