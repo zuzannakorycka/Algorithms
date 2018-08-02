5426b00bfbd551011a5507901a2f46ea7b169881
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-08-02 18:09:59 +0200

    Added four test in class ExponentiationTest for testing method exponentiationIteratively.

--- src/test/java/ExponentiationTest.java
+++ src/test/java/ExponentiationTest.java
@@ -28,7 +28,31 @@ public class ExponentiationTest {
 
     @Test
     public void shouldReturnReverseNAndPositiveEInFunctionRecursively(){
-        result = exponentiation.exponentiationRecursively(2, -1);
-        assertEquals(0.5, result, DELTA);
+        result = exponentiation.exponentiationRecursively(3, -1);
+        assertEquals((double)1/3, result, DELTA);
+    }
+
+    @Test
+    public void shouldReturnEightForParametersTwoAndThreeInMethodIteratively(){
+        result = exponentiation.exponentiationIteratively(2,3);
+        assertEquals(8,result, DELTA);
+    }
+
+    @Test
+    public void shouldReturnOneForExponentEEqualsZeroInFunctionIteratively(){
+        result = exponentiation.exponentiationIteratively(2,0);
+        assertEquals(1,result, DELTA);
+    }
+
+    @Test
+    public void shouldReturnNegativeNumberInFunctionIteriatively(){
+        result = exponentiation.exponentiationIteratively(-2, 3);
+        assertEquals(-8,result, DELTA);
+    }
+
+    @Test
+    public void shouldReturnReverseNAndPositiveEInFunctionIteratively(){
+        result = exponentiation.exponentiationIteratively(3, -1);
+        assertEquals((double) 1/3, result, DELTA);
     }
 }
